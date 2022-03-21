15b1596ef4c887f4e0d28a990e087472a8e4a515
senocax <you@example.com>
2022-03-21 13:01:36 -0300

    Create Tables

---
+++ table.sql
@@ -0,0 +1,15 @@
+CREATE TABLE agrupado(
+   fecha date PRIMARY KEY, 
+   cod_localidad INTEGER NOT NULL,
+   id_provincia INTEGER NOT NULL,
+   id_departamento INTEGER NOT NULL,
+   categoría VARCHAR(255) NOT NULL ,
+   provincia VARCHAR(255) NOT NULL,
+   localidad VARCHAR(255) NOT NULL,
+   nombre VARCHAR(255) NOT NULL,
+   domicilio VARCHAR(255) NOT NULL,
+   código VARCHAR(255) NOT NULL,
+   número VARCHAR(255) NOT NULL,
+   mail VARCHAR(255) NOT NULL,
+   web VARCHAR(255) NOT NULL
+);
